4fcdf67de6b0728ee91ffd44465d9b37483aa802
TienNguyen2608 <118101819+ht2608@users.noreply.github.com>
2024-09-08 21:47:47 +0700

    aaaaa

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,11 @@
 
 public class Calculator {
     public int add(int a, int b){
-        int rs = a + b;
+        int rs = a * b;
+        int rx = a * b;
+        int rv = a / b;
         return rs;
     }
+
+
 }
